e0466cd381a39b103a2bc6bd3181024f8293bc40
lucasV327 <lucas_vn95@hotmail.com>
2017-10-05 16:03:56 -0300

    Update autoload.php

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -3,4 +3,5 @@
 //Auto load para carregamento das classes
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
+//método getLoader
 return ComposerAutoloaderInitafdcab8c3136f2c15852bbea3dae0fd4::getLoader();
